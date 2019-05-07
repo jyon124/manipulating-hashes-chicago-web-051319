9db6dbb5203bc2f83ee50abd0e81665e507a4794
Jooyon Won <jyon1204@gmail.com>
2019-05-07 22:33:53 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -7,10 +7,10 @@ def second_challenge
   }
 
   #code your solution here!
+  empty_cart = ""
   groceries.each do |foods, values|
     values.each do |kinds|
       puts "#{kinds}"
     end
 end
-return "#{kinds}"
 end
